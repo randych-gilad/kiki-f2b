d8e640116c5aa70c7c6b358f6810a72a66b5827d
randych-gilad <xadaka@gmail.com>
2024-07-06 17:13:43 +0300

    db types

---
+++ db_types.go
@@ -0,0 +1,35 @@
+package main
+
+type Fail2banDb struct {
+	Version int `json:"version"`
+}
+
+type Jail struct {
+	Name    string `json:"name"`
+	Enabled int    `json:"enabled"`
+}
+
+type Log struct {
+	Jail         string `json:"jail"`
+	Path         string `json:"path"`
+	FirstLineMD5 string `json:"firstlinemd5"`
+	LastFilePos  int    `json:"lastfilepos"`
+}
+
+type Ban struct {
+	Jail      string `json:"jail"`
+	IP        string `json:"ip"`
+	TimeOfBan int    `json:"timeofban"`
+	BanTime   int    `json:"bantime"`
+	BanCount  int    `json:"bancount"`
+	Data      string `json:"data"`
+}
+
+type Bip struct {
+	IP        string `json:"ip"`
+	Jail      string `json:"jail"`
+	TimeOfBan int    `json:"timeofban"`
+	BanTime   int    `json:"bantime"`
+	BanCount  int    `json:"bancount"`
+	Data      string `json:"data"`
+}
